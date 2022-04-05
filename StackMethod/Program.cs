8369c979ff4fa7203de75284cd517337a41b2cc9
tu704vh99 <tu704vh99@214-12>
2022-04-05 12:32:04 +0400

    Add project files.

---
+++ StackMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static Queue<string> reserve(Queue<string> queue)
+        {
+            Stack<string> test = new Stack<string>();
+
+            foreach (var item in queue)
+            {
+                test.Push(item);
+            }
+
+            Queue<string> reservecopy = new Queue<string>();
+
+            foreach (var item in test)
+            {
+                reservecopy.Enqueue(item);
+            }
+
+            
+            return reservecopy;
+        }
+    }
+}
